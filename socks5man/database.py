e746172e068f65046822c3f0694d48cd98281f9d
doomedraven <doomedraven@users.noreply.github.com>
2019-10-17 10:18:52 +0200

    Update database.py

--- socks5man/database.py
+++ socks5man/database.py
@@ -7,6 +7,7 @@ from sqlalchemy import (
     Column, Integer, String, DateTime, Boolean, Text, create_engine,
     Float, and_, func
 )
+
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
